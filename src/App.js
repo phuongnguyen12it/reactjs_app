b9f1ef6c9ac1a0081d7a5c059e1df6ea6f8a66ef
PHUONGNV <phuongnv@soft-flight.work>
2019-05-08 14:33:19 +0700

    add project react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello world !!!
+      </div>
+    );
+  }
+}
+
+export default App;
